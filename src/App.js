5e393e13713f539b579b17519265a9afa41e8fbe
asmrayat <asmrayat@gmail.com>
2022-02-08 03:04:09 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import OrderReview from './components/OrderReview/OrderReview'
+import Product from './components/Product/Product';
+import Inventory from './components/Inventory/Inventory';
+import NotFound from './components/Notfound/NotFound';
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Shop></Shop>
+          </Route>
+          <Route path="/shop">
+            <Shop></Shop>
+          </Route>
+          <Route path="/review">
+            <OrderReview></OrderReview>
+          </Route>
+          <Route path="/inventory">
+            <Inventory></Inventory>
+          </Route>
+          <Route>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
+    //npm install react-router-dom@5.2.0
   );
 }
 
